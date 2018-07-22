833f6bff914534d16ace4149e0031e799bbbb4cc
prashant kaushik <prashant@ovalelabs.com>
2018-07-22 17:18:08 +0530

    first

---
+++ hello.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env nodejs
+var fs = require('fs');
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(8100, 'localhost');
+console.log('Server running at http://localhost:8080/');
+
+
+
+
+
